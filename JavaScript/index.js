ba7e16146cf2be1a83256f45849f8eaa141c9e99
Tobby Willson <adepitantobi@gmail.com>
2024-06-11 18:02:58 +0100

    Initial Commit

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,3 +2,19 @@
 const year = document.querySelector(".year");
 const newDate = new Date().getFullYear();
 year.textContent = newDate;
+
+// toggling hamburger menu
+const icon = document.querySelector("img");
+const menu = document.querySelector(".menu");
+
+function change() {
+  menu.classList.toggle("show");
+
+  if (menu.classList.contains("show")) {
+    icon.src = "images/X-icon.png";
+  } else {
+    icon.src = "images/Hamburger-Line.png";
+  }
+}
+
+icon.addEventListener("click", change);
