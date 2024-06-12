8d9517443e3c8c0a8768761a9a99be9530deea1a
Tobby Willson <adepitantobi@gmail.com>
2024-06-12 22:19:31 +0100

    Initial Commit

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,17 +4,23 @@ const newDate = new Date().getFullYear();
 year.textContent = newDate;
 
 // toggling hamburger menu
-const icon = document.querySelector("img");
+const hamBtn = document.querySelector(".ham-btn");
 const menu = document.querySelector(".menu");
+const icon = document.querySelector(".icon");
 
 function change() {
   menu.classList.toggle("show");
-
-  if (menu.classList.contains("show")) {
-    icon.src = "images/X-icon.png";
-  } else {
-    icon.src = "images/Hamburger-Line.png";
-  }
+  icon.classList.toggle("fa-times");
 }
 
-icon.addEventListener("click", change);
+hamBtn.addEventListener("click", change);
+
+// toggling more-tabs on and off
+const moreTab = document.querySelector(".more-tab");
+const otherTabs = document.querySelector(".other-tabs");
+const arrow = document.querySelector(".arrow");
+
+moreTab.addEventListener("click", () => {
+  otherTabs.classList.toggle("show");
+  arrow.classList.toggle("fa-angle-up");
+});
