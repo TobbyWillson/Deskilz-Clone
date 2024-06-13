2e14835b38d2d00736db19a4154bde65fbfce48f
Tobby Willson <adepitantobi@gmail.com>
2024-06-13 17:49:54 +0100

    initial commit

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -28,16 +28,40 @@ moreTab.addEventListener("click", () => {
 // Checking for the date
 const day = new Date();
 const dayOfWeek = day.getDay();
+const time = day.getHours();
 
 const lists = document.querySelectorAll(".location li");
 
-if (dayOfWeek === 6 || day === 7) {
+if (dayOfWeek === 7 && time >= 20) {
   lists[1].innerHTML = `<li>
             <i class="fas fa-clock"></i>
             <span class="weekend">We are closed, check back on Monday</span>
           </li>`;
 
   lists[2].textContent = "";
+} else if (dayOfWeek === 7 && time < 14) {
+  lists[1].innerHTML = `<li>
+            <i class="fas fa-clock"></i>
+            <span class="weekend">Opening hour is 2PM</span>
+          </li>`;
+
+  lists[2].textContent = "";
+} else if (dayOfWeek <= 6 && time >= 20) {
+  lists[1].innerHTML = `<li>
+            <i class="fas fa-clock"></i>
+            <span class="weekend">We are closed, check back tomorrow</span>
+          </li>`;
+
+  lists[2].textContent = "";
+} else if (dayOfWeek <= 6 && time < 8) {
+  lists[1].innerHTML = `<li>
+            <i class="fas fa-clock"></i>
+            <span class="weekend">Opening hour is 8AM</span>
+          </li>`;
+
+  lists[2].textContent = "";
+} else {
+  console.log();
 }
 
 // Add to cart
@@ -45,10 +69,16 @@ const cardIcons = document.querySelectorAll(".card .icon");
 
 cardIcons.forEach((item) => {
   item.addEventListener("click", () => {
-    if (dayOfWeek === 6 || dayOfWeek === 7) {
-      alert("check back on Monday");
+    if (dayOfWeek === 7 && time >= 20) {
+      alert("Check back on Monday");
+    } else if (dayOfWeek === 7 && time < 14) {
+      alert("Opening hour is 2PM");
+    } else if (dayOfWeek <= 6 && time < 8) {
+      alert("Opening hour is 8AM");
+    } else if (dayOfWeek <= 6 && time >= 20) {
+      alert("We are closed, check back tomorrow");
     } else {
-      alert("No problems, keep ordering");
+      alert("1 more item added to cart");
     }
   });
 });
